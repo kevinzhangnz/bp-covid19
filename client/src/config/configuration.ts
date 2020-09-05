a46be8d2152b00e1ab4e21abad2266f96bc5eb43
kevinzhangnz <51142004+kevinzhangnz@users.noreply.github.com>
2020-09-05 22:00:13 +1200

    add configuration for the service

---
+++ client/src/config/configuration.ts
@@ -0,0 +1,8 @@
+export class Configuration {
+    readonly HOST = 'https://api.covid19api.com';
+    readonly COUNTRIES_PATH = '/countries';
+    readonly COUNTRY_PATH = '/country';
+    readonly SUMMARY_PATH = '/summary';
+
+    readonly TIMEOUT = 5000;
+}
